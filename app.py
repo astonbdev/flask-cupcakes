fa419745e4ff755b40d2647f8901b036fe5fec51
Brian Aston <astonb.dev@gmail.com>
2022-02-07 12:34:42 -0800

    Finished first 3 routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 import os
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, jsonify, request
 # from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Cupcake
@@ -13,7 +13,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 db.create_all()
 
-app.config['SECRET_KEY'] = os.environ["APP_SECRET_KEY"]
+app.config['SECRET_KEY'] = "test"
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -22,3 +22,40 @@ app.config['SECRET_KEY'] = os.environ["APP_SECRET_KEY"]
 
 # debug = DebugToolbarExtension(app)
 
+
+@app.get("/api/cupcakes")
+def show_all_cupcakes():
+
+    cupcakes = Cupcake.query.all()
+
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def show_cupcake(cupcake_id):
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+
+    cupcake = Cupcake(
+        flavor=request.json["flavor"],
+        size=request.json["size"],
+        rating=request.json["rating"],
+        image=request.json["image"]
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized = cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Cupcake(db.Model):
 
     __tablename__ = "cupcakes"
@@ -30,6 +31,16 @@ class Cupcake(db.Model):
         nullable=False,
         default="https://tinyurl.com/demo-cupcake",
     )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
 
 def connect_db(app):
     """Connect to database."""
