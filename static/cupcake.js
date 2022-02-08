01dc0629a1278a4f5e9d507d0dec6ebe39675e02
edwinkim97 <edwinkim124@gmail.com>
2022-02-07 17:56:53 -0800

    finished last step

--- static/cupcake.js
+++ static/cupcake.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const CUPCAKE_URL = "http://localhost:5001/api";
+const CUPCAKE_URL = "http://localhost:5000/api";
 
 let CUPCAKES_LIST = "";
 
+/** Gets cupcakes JSON from server and calls displayCupcakes */
 async function getCupcakes(){
     console.debug("getCupcakes")
 
@@ -16,6 +17,7 @@ async function getCupcakes(){
     CUPCAKES_LIST = response.data.cupcakes;
 }
 
+/** Renders cupcakes from cupcakes JSON */
 function displayCupcakes(cupcakes){
     console.debug("displayCupcakes")
 
@@ -34,5 +36,35 @@ function displayCupcakes(cupcakes){
     }
 }
 
+/** Handles cupcake form submissions and adds to server database */
+async function addCupcake(evt){
+    evt.preventDefault();
+
+    const flavor = $("#flavor").val();
+    const size = $("#size").val();
+    const rating = $("#rating").val();
+    const imgURL = $("#imgURL").val();
+
+    const response = await axios.post(CUPCAKE_URL + "/cupcakes",
+    {flavor:flavor, size:size, rating:rating, image:imgURL});
+    
+    addCupcakeHTML(response);
+}
+
+/** Appends newly added cupcake to DOM list */
+function addCupcakeHTML(response){
+    const cupcake = response.data.cupcake;
+
+    $("#cupcake-list").append($(
+        `<li>
+        <img src = ${cupcake.image}>
+        <p>Flavor: ${cupcake.flavor}</p>
+        <p>Rating: ${cupcake.rating}</p>
+        <p>Size: ${cupcake.size}</p>
+        </li>`
+    ))
+}
+
+$("#cupcake-submit").on("click", addCupcake)
 
 getCupcakes();
